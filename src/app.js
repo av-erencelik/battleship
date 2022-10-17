0d373d37f212f93d36b607211c71834d86bec32f
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-10-17 15:12:40 +0300

    start button event listener

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
 import "./styles.scss";
+
+const startButton = document.getElementById("start");
+startButton.addEventListener("click", () => {
+  startButton.style.translate = "1400px";
+  setTimeout(function x() {
+    startButton.style.display = "none";
+  }, 800);
+});

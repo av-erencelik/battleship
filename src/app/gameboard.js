0a577859369bdad54d30cdfad0aadc43f1da6e0c
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-10-19 08:52:26 +0300

    randomly generate ships coordinates

--- src/app/gameboard.js
+++ src/app/gameboard.js
@@ -21,51 +21,108 @@ export function gameBoard() {
   let ship1_2 = Ship(1);
   let ship1_3 = Ship(1);
   let ship1_4 = Ship(1);
+  board.placeShips4 = function () {
+    let i = Math.floor(Math.random() * 100);
+    const lastDigit2Str = String(i).slice(-1);
+    const lastDigit2Num = Number(lastDigit2Str);
+    if (lastDigit2Num >= 7) {
+      board.board[i] = ship4;
+      board.board[i - 1] = ship4;
+      board.board[i - 2] = ship4;
+      board.board[i - 3] = ship4;
+      board.shipCoordinates.push(i);
+      board.shipCoordinates.push(i - 1);
+      board.shipCoordinates.push(i - 2);
+      board.shipCoordinates.push(i - 3);
+    } else {
+      board.board[i] = ship4;
+      board.board[i + 1] = ship4;
+      board.board[i + 2] = ship4;
+      board.board[i + 3] = ship4;
+      board.shipCoordinates.push(i);
+      board.shipCoordinates.push(i + 1);
+      board.shipCoordinates.push(i + 2);
+      board.shipCoordinates.push(i + 3);
+    }
+  };
+  board.placeShips3 = function (ship) {
+    let i = Math.floor(Math.random() * 100);
+    const lastDigit2Str = String(i).slice(-1);
+    const lastDigit2Num = Number(lastDigit2Str);
+    if (lastDigit2Num >= 8) {
+      if (board.board[i] || board.board[i - 1] || board.board[i - 2]) {
+        this.placeShips3(ship);
+        return;
+      } else {
+        board.board[i] = ship;
+        board.board[i - 1] = ship;
+        board.board[i - 2] = ship;
+        board.shipCoordinates.push(i);
+        board.shipCoordinates.push(i - 1);
+        board.shipCoordinates.push(i - 2);
+      }
+    } else {
+      if (board.board[i] || board.board[i + 1] || board.board[i + 2]) {
+        this.placeShips3(ship);
+        return;
+      } else {
+        board.board[i] = ship;
+        board.board[i + 1] = ship;
+        board.board[i + 2] = ship;
+        board.shipCoordinates.push(i);
+        board.shipCoordinates.push(i + 1);
+        board.shipCoordinates.push(i + 2);
+      }
+    }
+  };
+  board.placeShips2 = function (ship) {
+    let i = Math.floor(Math.random() * 100);
+    const lastDigit2Str = String(i).slice(-1);
+    const lastDigit2Num = Number(lastDigit2Str);
+    if (lastDigit2Num == 9) {
+      if (board.board[i] || board.board[i - 1]) {
+        this.placeShips2(ship);
+        return;
+      } else {
+        board.board[i] = ship;
+        board.board[i - 1] = ship;
+        board.shipCoordinates.push(i);
+        board.shipCoordinates.push(i - 1);
+      }
+    } else {
+      if (board.board[i] || board.board[i + 1]) {
+        this.placeShips2();
+        return;
+      } else {
+        board.board[i] = ship;
+        board.board[i + 1] = ship;
+        board.shipCoordinates.push(i);
+        board.shipCoordinates.push(i + 1);
+      }
+    }
+  };
+  board.placeShips1 = function (ship) {
+    let i = Math.floor(Math.random() * 100);
+    const lastDigit2Str = String(i).slice(-1);
+    const lastDigit2Num = Number(lastDigit2Str);
+    if (board.board[i]) {
+      this.placeShips1(ship);
+    } else {
+      board.board[i] = ship;
+      board.shipCoordinates.push(i);
+    }
+  };
   board.placeShips = function () {
-    // ship with length 4
-    board.board[3] = ship4;
-    board.shipCoordinates.push(3);
-    board.board[4] = ship4;
-    board.shipCoordinates.push(4);
-    board.board[5] = ship4;
-    board.shipCoordinates.push(5);
-    board.board[6] = ship4;
-    board.shipCoordinates.push(6);
-    // ships with length 3s
-    board.board[26] = ship3_1;
-    board.shipCoordinates.push(26);
-    board.board[27] = ship3_1;
-    board.shipCoordinates.push(27);
-    board.board[28] = ship3_1;
-    board.shipCoordinates.push(28);
-    board.board[60] = ship3_2;
-    board.shipCoordinates.push(60);
-    board.board[61] = ship3_2;
-    board.shipCoordinates.push(61);
-    board.board[62] = ship3_2;
-    board.shipCoordinates.push(62);
-    // ships with length 2s
-    board.board[41] = ship2_1;
-    board.shipCoordinates.push(41);
-    board.board[42] = ship2_1;
-    board.shipCoordinates.push(42);
-    board.board[48] = ship2_2;
-    board.shipCoordinates.push(48);
-    board.board[49] = ship2_2;
-    board.shipCoordinates.push(49);
-    board.board[95] = ship2_3;
-    board.shipCoordinates.push(95);
-    board.board[96] = ship2_3;
-    board.shipCoordinates.push(96);
-    // ships with length 1s
-    board.board[77] = ship1_1;
-    board.shipCoordinates.push(77);
-    board.board[33] = ship1_2;
-    board.shipCoordinates.push(33);
-    board.board[55] = ship1_3;
-    board.shipCoordinates.push(55);
-    board.board[11] = ship1_4;
-    board.shipCoordinates.push(11);
+    this.placeShips4();
+    this.placeShips3(ship3_1);
+    this.placeShips3(ship3_2);
+    this.placeShips2(ship2_1);
+    this.placeShips2(ship2_2);
+    this.placeShips2(ship2_3);
+    this.placeShips1(ship1_1);
+    this.placeShips1(ship1_2);
+    this.placeShips1(ship1_3);
+    this.placeShips1(ship1_4);
   };
   board.receiveAttack = function (coordinate) {
     if (

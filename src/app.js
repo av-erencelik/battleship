451a60230702a799cf5ec854801161e7fc389f7d
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-10-19 11:09:44 +0300

    add song and stop button for song

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import { game } from "./app/game";
 import "./styles.scss";
-
+let mainSong = document.querySelector("#main");
+let soundButton = document.getElementById("sound");
 const startButton = document.getElementById("start");
 startButton.addEventListener("click", () => {
+  mainSong.volume = 0.5;
+  mainSong.play();
   startButton.style.translate = "1400px";
   setTimeout(function x() {
     startButton.style.display = "none";
   }, 800);
   game();
+});
+soundButton.addEventListener("click", () => {
+  if (mainSong.volume == 0.5) {
+    mainSong.volume = 0.0;
+  } else {
+    mainSong.volume = 0.5;
+  }
 });
